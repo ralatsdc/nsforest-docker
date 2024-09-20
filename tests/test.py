9b40cf056f154b30348ea0df05184ea7b3a29568
spearw <spearw@gmail.com>
2024-09-20 13:34:20 -0400

    Run black on test.py

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,8 @@
 import subprocess
 import unittest
 
+
 class TestNSForestContainer(unittest.TestCase):
-
-
     def test_consistency(self):
 
         # Set up variables
@@ -15,22 +14,28 @@ class TestNSForestContainer(unittest.TestCase):
         ns_forest_path = "/root/NSForest/nsforest.py"
 
         # Run GOEnrich
-        subprocess.run(["python",
-                        ns_forest_path,
-                        adata_path,
-                        "--run-nsforest-on-file",
-                        "-c", "cluster",
-                        "-d", data_path])
+        subprocess.run(
+            [
+                "python",
+                ns_forest_path,
+                adata_path,
+                "--run-nsforest-on-file",
+                "-c",
+                "cluster",
+                "-d",
+                data_path,
+            ]
+        )
 
         # Compare output with expected output
         with open(output_path) as output:
             with open(expected_output_path) as expected:
                 self.assertListEqual(list(output), list(expected))
 
-
         # Clean up
         subprocess.run(["rm", output_path])
         subprocess.run(["rm", pp_path])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
