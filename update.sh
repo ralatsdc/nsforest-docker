19361ef1284c455388ee1272d04736a6babcb734
Raymond LeClair <raymond.leclair@gmail.com>
2025-01-31 16:44:36 -0500

    Update continuumio/miniconda3 version

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Run this script in base image:
-# $ docker run -itv $PWD:/root -w /root --platform linux/amd64 continuumio/miniconda3@sha256:166ff37fba6c25fcad8516aa5481a2a8dfde11370f81b245c1e2e8002e68bcce
+# $ docker run -itv $PWD:/root -w /root --platform linux/amd64 continuumio/miniconda3:24.11.1-0
 eval "$(conda shell.bash hook)"
 conda env create -f update.yaml
 conda activate nsforest
